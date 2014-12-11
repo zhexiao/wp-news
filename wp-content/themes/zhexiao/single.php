bd58691776510136a6dcd9d250d069f0ce2d8f37
Andy Xiao <zhexiao@163.com>
2014-12-11 10:53:37 -0500

    add single

---
+++ wp-content/themes/zhexiao/single.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The Template for displaying all single post
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<nav class="nav-single">
+					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+				</nav>
+
+				<?php comments_template( '', true ); ?>
+
+			<?php endwhile; ?>
+
+		</div>
+	</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
