ee9e49e5aa8e834a0ee5b24b5472a0938d4a48b4
Andy Xiao <zhexiao@163.com>
2014-12-11 17:45:33 -0500

    add search page

---
+++ wp-content/themes/zhexiao/search.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying Search Results pages
+ */
+
+get_header(); ?>
+
+<div class="main-top-3">
+	<div class="col-md-8">
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1 class="page-title">
+					<?php printf( 'Search Results for: %s', '<span>' . get_search_query() . '</span>' ); ?>
+				</h1>
+			</header>
+
+			<?php post_content_nav( 'nav-above' ); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
+
+			<?php post_content_nav( 'nav-below' ); ?>
+
+		<?php else : ?>
+			<article id="post-0" class="post no-results not-found">
+				<header class="entry-header">
+					<h1 class="entry-title">Nothing Found</h1>
+				</header>
+
+				<div class="entry-content">
+					<p>Sorry, but nothing matched your search criteria. Please try again with some different keywords.'</p>
+				</div>
+			</article>
+		<?php endif; ?>
+	</div>
+	
+	<div class="col-md-4 col-right-sidebar">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
