9eb4ed51d553c39ded63d1b2771e1484ebe38cbd
Zhe Xiao <zhexiao@163.com>
2015-04-18 00:09:17 -0400

    add bing webmaster

--- wp-content/themes/zhexiao/header.php
+++ wp-content/themes/zhexiao/header.php
@@ -4,7 +4,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="google-site-verification" content="WQrSIH1TczWg_pgwR61ccKYSqtFOeRkcmuVjcQz_-NU" />
-	<meta name="msvalidate.01" content="D3C5A606A3F775DE03395334681397D9" />
+	<meta name="msvalidate.01" content="793991BF5868EC482D178E28926211BE" />
 	<title><?=wp_title( '', false, 'right' );?></title>
 	<?php wp_head(); ?>
 </head>
